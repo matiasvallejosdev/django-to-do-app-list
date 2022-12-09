1734f936f72f7f963d1de29043880dfb7c40276a
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-08 21:24:35 -0300

    Update dbsqlite as root project

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -76,14 +76,20 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'djangotodoapp',
+#         'USER': 'todoapp',
+#         'PASSWORD': '1986',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'djangotodoapp',
-        'USER': 'todoapp',
-        'PASSWORD': '1986',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
