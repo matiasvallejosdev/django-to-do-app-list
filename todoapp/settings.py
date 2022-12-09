5278d0e5a7a259a5714d407def085ea253cac177
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-09 09:45:34 -0300

    Delete secret key and create it private

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -10,8 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+from dotenv import load_dotenv
 from pathlib import Path
 import os
+
+# Load enviroment .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=9*zx3vtuo-z9xdh+_)hs&5q!(yy$1!cbh)%c6frv(w1*4^v8'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
