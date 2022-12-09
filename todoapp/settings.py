b7b6b99f5ae69f077af8434503d2a513f2e26045
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-09 09:46:22 -0300

    Base dir resolved

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -14,12 +14,11 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
-# Load enviroment .env
-load_dotenv(os.path.join(BASE_DIR, ".env"))
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Load enviroment .env
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
