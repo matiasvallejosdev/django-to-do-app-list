899058453463cbe4ca7e386d1c3e07d7a04f8ab0
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-08 21:58:44 -0300

    Csrf securiy origin

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -28,7 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['web-production-e8df.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['https://web-production-e8df.up.railway.app']
+
 # Application definition
 
 INSTALLED_APPS = [
