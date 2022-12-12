039c160c669a7b9b2b98351bffa3b0c3c8234cbe
Matias Vallejos <matiasvallejosdev@outlook.com>
2022-12-12 12:33:37 -0300

    Update settings to redeploy

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,6 +27,7 @@ dotenv.load_dotenv(dotenv_path)
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+# Getting from the enviroment
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
